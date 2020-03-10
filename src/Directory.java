95a08b4cf1d988f7dbb129af2463b8e1e4731985
dcwell <cwell.de@gmail.com>
2020-03-10 13:28:06 -0700

    Added comments to directory file

--- src/Directory.java
+++ src/Directory.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Directory {
     private static int maxChars = 30;
 
-    private int fsize[];
-    private char fnames[][];
+    private int fsize[];    // each element stores a different file size.
+    private char fnames[][];    // each element stores a different file name.
 
     public Directory(int maxInumber) {
         fsizes = new int[maxInumber];
@@ -18,22 +18,28 @@ public class Directory {
     }
 
     public void bytes2directory(byte data[]) {
-
+        // assumes data[] received directory information from disk
+        // initializes the Directory instance with this data[]
     }
 
     public byte[] directory2bytes() {
-
+        // converts and return Directory information into a plain byte array
+        // this byte array will be written back to disk
+        // note: only meaningfull directory information should be converted
+        // into bytes.
     }
 
     public short ialloc(String filename) {
-
+        // filename is the one of a file to be created.
+        // allocates a new inode number for this filename
     }
-    
-    public boolean ifree(short iNum) {
 
+    public boolean ifree(short iNum) {
+        // deallocates this inumber (inode number)
+        // the corresponding file will be deleted.
     }
 
     public short iname(String filename) {
-
+        // returns the inumber corresponding to this filename
     }
 }
