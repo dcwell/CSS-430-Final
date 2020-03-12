9b4b71d189b37dced7dea0386e4a19fc9b605b8e
dcwell <cwell.de@gmail.com>
2020-03-12 15:21:22 -0700

    SuperBlock.java added

---
+++ src/SuperBlock.java
@@ -0,0 +1,33 @@
+/**
+ * @authors Denali Cornwell & Jayden Stipek.
+ */
+public class SuperBlock {
+    private final int defaultInodeBlocks = 64;
+    public int totalBlocks;
+    public int inodeBlocks;
+    public int freeList;
+
+    public SuperBlock(int i) {
+
+    }
+
+    public void sync() {
+
+    }
+
+    public void format() {
+
+    }
+
+    public void format(int i) {
+
+    }
+
+    public int getFreeBlock() {
+
+    }
+
+    public boolean returnBlock(int i) {
+
+    }
+}
