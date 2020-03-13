8ed7a1b116369ef5446059dc56d71a58f5ef260d
Clause828 <55769079+Clause828@users.noreply.github.com>
2020-03-12 22:46:16 -0700

    Update SuperBlock.java

--- src/SuperBlock.java
+++ src/SuperBlock.java
@@ -26,19 +26,23 @@ public class SuperBlock {
     }
 
     public void sync() {
-
+        byte [] superBlock = new byte[Disk.blockSize];
+        SysLib.int2bytes(totalBlocks, superBlock, 0);
+        SysLib.int2bytes(inodeBlocks, superBlock, 4);
+        SysLib.int2bytes(freeList, superBlock, 8);
+        SysLib.rawwrite(0,superBlock);
     }
 
     public void format() {
 
     }
 
-    public void format(int i) {
+    public void format(int totalInodes) {
 
     }
 
     public int getFreeBlock() {
-        return -1;
+        return freeList++;
     }
 
     public boolean returnBlock(int i) {
