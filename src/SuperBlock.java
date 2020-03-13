30d38acf487c5161ac37ae82c8bc9114120d1817
Clause828 <55769079+Clause828@users.noreply.github.com>
2020-03-12 17:53:12 -0700

    Finished Implementing SuperBlock

--- src/SuperBlock.java
+++ src/SuperBlock.java
@@ -4,12 +4,25 @@
 public class SuperBlock {
     private final int defaultInodeBlocks = 64;
     public int totalBlocks;
-    public int inodeBlocks;
+    public int totalINodes;
     public int freeList;
 
-    public SuperBlock(int i) {
-        this.totalBlocks =
+    public SuperBlock(int diskSize) {
+        //read the superblock from disk
+        byte [] superBlock = new byte[Disk.blockSize];
+        SysLib.rawread(0,superBlock);
+        totalBlocks = SysLib.bytes2int(superBlock, 0);
+        totalINodes = SysLib.bytes2int(superBlock,4);
+        freeList = SysLib.bytes2int(superBlock, 8 );
 
+        if(totalBlocks == diskSize && totalINodes > 0 && freeList >= 2)
+            //disk contents are valid
+            return;
+        else {
+            //need to format disk
+            totalBlocks = diskSize;
+            format(defaultInodeBlocks);
+        }
     }
 
     public void sync() {
@@ -25,10 +38,10 @@ public class SuperBlock {
     }
 
     public int getFreeBlock() {
-
+        return -1;
     }
 
     public boolean returnBlock(int i) {
-
+        return false;
     }
 }
