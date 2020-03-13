81647f2c0efcad025af02c9693bc2237e1ad2180
Clause828 <55769079+Clause828@users.noreply.github.com>
2020-03-13 13:56:50 -0700

    Organized and made better

--- src/Inode.java
+++ src/Inode.java
@@ -15,7 +15,7 @@ public class Inode {
         length = 0;
         count = 0;
         flag = 1;
-        for(int i = 0; i < directSize; i++) {
+        for (int i = 0; i < directSize; i++) {
             direct[i] = -1;
         }
         indirect = -1;
@@ -32,36 +32,30 @@ public class Inode {
         offset += 2;
         flag = SysLib.bytes2short(data, offset);
         offset += 2;
-        if(blockNumber < 11) { //direct blocks
-            for (int i = 0; i < directSize; i++) {
-                direct[i] = SysLib.bytes2short(data, offset);
-                offset += 2;
-            }
-            indirect = SysLib.bytes2short(data, offset);
-        }else
-        {
-            for(int i  = indirect; i < indirect + 256; i++) //for if you are using indirect block
-            {
-                SysLib.bytes2int(data,offset);
-                SysLib.rawwrite(i,data);
-            }
+        for (int i = 0; i < directSize; i++) {
+            direct[i] = SysLib.bytes2short(data, offset);
+            offset += 2;
         }
+        indirect = SysLib.bytes2short(data, offset);
+        offset += 4;
     }
 
     /**
-     * Save to disk as the i-th inode
+     * Save to disk as the i-th inodes
+     *
      * @param i
      */
     public void toDisk(short iNumber) {
         byte[] data = new byte[Disk.blockSize];
         int offset = (iNumber % 16) * 32;
         SysLib.short2bytes(iNumber, data, offset);
-        SysLib.rawwrite(iNumber,data);
+        SysLib.rawwrite(iNumber, data);
     }
 
 
     /**
      * Finds the Index Block Number
+     *
      * @return Index Block
      */
     public int findIndexBlock() {
@@ -83,7 +77,7 @@ public class Inode {
 
         if (indirect == -1) //check if indirect block can be used
             return false; //if so fail
-         else {
+        else {
 
             //set up a byte array to be written to index block
             byte[] indexSetUp = new byte[Disk.blockSize];
@@ -100,39 +94,50 @@ public class Inode {
         }
     }
 
+    /**
+     * @param iNumber
+     * @return
+     */
     public int findTargetBlock(int iNumber) {
-        if(iNumber < 0 )
+        if (iNumber < 0)
             return -1;
         else
             return (iNumber % 16) * 32;
     }
 
+    /**
+     * @param i
+     * @param i1
+     * @return
+     */
     public int registerTargetBlock(int i, short i1) {
         if (i == -1)  //check if indirect block can be used
             return i; //if so fail
 
+        //set up a byte array to be written to index block
+        byte[] indexSetUp = new byte[Disk.blockSize];
+        int offset = 0;
+
+        for (int index = 0; index < (Disk.blockSize / 2); index++) {
+            SysLib.short2bytes((short) i1, indexSetUp, offset);
+            offset += 2;
+        }
+        SysLib.rawwrite(i, indexSetUp);
+        return i;
+    }
+
+    /**
+     * @return
+     */
+    public byte[] unregisterIndexBlock() {
+        if (indirect > -1) { //if it is actually being used
             //set up a byte array to be written to index block
             byte[] indexSetUp = new byte[Disk.blockSize];
             int offset = 0;
-
-            for (int index = 0; index < (Disk.blockSize / 2); index++) {
-                SysLib.short2bytes((short) i1, indexSetUp, offset);
-                offset += 2;
-            }
-
-            SysLib.rawwrite(i, indexSetUp);
-            return i;
-    }
-
-    public byte[] unregisterIndexBlock() {
-            //set up a byte array to be written to index block
-            byte[] indexSetUp = new byte[Disk.blockSize];
-            if(indirect > -1) { //if it is actually being used
-                int offset = 0;
-                SysLib.rawread(indirect, indexSetUp); //read up all the data inside the block
-                indirect = -1; //reset it to not being used
-                return indexSetUp;
-            }
-            return null;
+            SysLib.rawread(indirect, indexSetUp); //read up all the data inside the block
+            indirect = -1; //reset it to not being used
+            return indexSetUp;
+        }
+        return null;
     }
 }
