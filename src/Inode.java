3df2cf9f56e6cb116512692796f6981209a24e82
Clause828 <55769079+Clause828@users.noreply.github.com>
2020-03-13 23:16:15 -0700

    inode version

--- src/Inode.java
+++ src/Inode.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 public class Inode {
     public static final int iNodeSize = 32;
     public static final int directSize = 11;
@@ -10,147 +5,198 @@ public class Inode {
     public static final int ErrorBlockRegistered = -1;
     public static final int ErrorPrecBlockUnused = -2;
     public static final int ErrorIndirectNull = -3;
-    public int length;
-    public short count;
-    public short flag;
-    public short[] direct = new short[11];
-    public short indirect;
+    public int length;                              //file size in bytes
+    public short count;                             //# of file-table entries pointing to this
+    public short flag;                              //an indirect pointer
+    public short[] direct = new short[directSize];  //direct pointers
+    public short indirect;                          //indirect pointer
 
-    Inode() {
-        this.length = 0;
-        this.count = 0;
-        this.flag = 1;
+    /**
+     * Default constructor for an Inode
+     */
+    public Inode() {
+        //SysLib.cout("\n**************** CALLING INODE DEFAULT CONST *******************\n");
+        length = 0;
+        count = 0;
+        flag = 1;
+        for (int i = 0; i < directSize; i++) {
+            direct[i] = -1;
+        }
+        indirect = -1;
+    }
 
-        for(int var1 = 0; var1 < 11; ++var1) {
-            this.direct[var1] = -1;
+    /**
+     * Secondary contructor for Inode, will construct an Inode with an iNumber.
+     *
+     * @param iNumber
+     */
+    public Inode(short iNumber) {
+        //SysLib.cout("\n**************** CALLING INODE ONE ARG CONST *******************\n");
+
+        int blockNumber = 1 + iNumber / 16;
+        byte[] data = new byte[Disk.blockSize];
+        SysLib.rawread(blockNumber, data);
+        int offset = (iNumber % 16) * 32;
+        length = SysLib.bytes2int(data, offset);
+        offset += 4;
+        count = SysLib.bytes2short(data, offset);
+        offset += 2;
+        flag = SysLib.bytes2short(data, offset);
+        offset += 2;
+        for (int i = 0; i < directSize; ++i) {
+            direct[i] = SysLib.bytes2short(data, offset);
+            offset += 2;
+        }
+        indirect = SysLib.bytes2short(data, offset);
+        offset+= 2;
+    }
+
+    /**
+     * Save to disk as the i-th inodes
+     * Move memory into disk using the inumber Value
+     * @param i
+     */
+    public void toDisk(short iNumber) {
+        //SysLib.cout("\n**************** CALLING INODE TODISK *******************\n");
+
+        if(iNumber < 0)
+            return;
+        //only needs 32 bytes for a single inode
+        byte[] data = new byte[32];
+        byte offset = 0;
+        //write the length
+        SysLib.int2bytes(length, data, offset);
+        int offsetInt = offset + 4;
+        //write the count
+        SysLib.short2bytes(count, data, offsetInt);
+        offsetInt += 2;
+        //write the flag
+        SysLib.short2bytes(flag, data, offsetInt);
+        offsetInt +=2;
+
+        //write block number that direct pointer points to
+        int pointerIndex;
+        for(pointerIndex = 0; pointerIndex < directSize; pointerIndex++)
+        {
+            SysLib.short2bytes(direct[pointerIndex], data, offsetInt);
+            offsetInt += 2;
+        }
+        SysLib.short2bytes(indirect, data, offsetInt);
+
+        //find the Inode we are on
+        pointerIndex = 1 + iNumber/16;
+        byte[] nodeData = new byte[Disk.blockSize];
+        SysLib.rawread(pointerIndex,nodeData);
+        offsetInt = (iNumber % 16) * 32;
+
+        //write it back to disk
+        System.arraycopy(data,0,nodeData,offsetInt,32);
+        SysLib.rawwrite(pointerIndex, nodeData);
+    }
+
+
+    /**
+     * returns the indirect block number because it is the same
+     *
+     * @return Index Block
+     */
+    public int findIndexBlock() {
+        //SysLib.cout("\n**************** CALLING INODE FIND INDEX BLOCK *******************\n");
+        return indirect;
+    }
+
+    /**
+     * Sets up the indrect block so it can be used by the file system. Only called once needed by the system.
+     *
+     * @param indexBlock The block number of the index block.
+     * @return true uf success, false if there are direct block that can still be used.
+     */
+    public boolean registerIndexBlock(short indexBlock) {
+        //SysLib.cout("\n**************** CALLING INODE REGISTER INDEX BLOCK *******************\n");
+        for (int i = 0; i < directSize; i++) { //check if any direct blocks can be used
+            if (direct[i] == -1)
+                return false; //if so fail.
         }
 
-        this.indirect = -1;
-    }
+        if (indirect != -1) //check if indirect block can be used
+            return false; //if so fail
+        else {
+            indirect = indexBlock;
+            //set up a byte array to be written to index block
+            byte[] indexSetUp = new byte[Disk.blockSize];
 
-    Inode(short var1) {
-        int var2 = 1 + var1 / 16;
-        byte[] var3 = new byte[512];
-        SysLib.rawread(var2, var3);
-        int var4 = var1 % 16 * 32;
-        this.length = SysLib.bytes2int(var3, var4);
-        var4 += 4;
-        this.count = SysLib.bytes2short(var3, var4);
-        var4 += 2;
-        this.flag = SysLib.bytes2short(var3, var4);
-        var4 += 2;
-
-        for(int var5 = 0; var5 < 11; ++var5) {
-            this.direct[var5] = SysLib.bytes2short(var3, var4);
-            var4 += 2;
-        }
-
-        this.indirect = SysLib.bytes2short(var3, var4);
-        var4 += 2;
-    }
-
-    void toDisk(short var1) {
-        byte[] var2 = new byte[32];
-        byte var3 = 0;
-        SysLib.int2bytes(this.length, var2, var3);
-        int var6 = var3 + 4;
-        SysLib.short2bytes(this.count, var2, var6);
-        var6 += 2;
-        SysLib.short2bytes(this.flag, var2, var6);
-        var6 += 2;
-
-        int var4;
-        for(var4 = 0; var4 < 11; ++var4) {
-            SysLib.short2bytes(this.direct[var4], var2, var6);
-            var6 += 2;
-        }
-
-        SysLib.short2bytes(this.indirect, var2, var6);
-        var6 += 2;
-        var4 = 1 + var1 / 16;
-        byte[] var5 = new byte[512];
-        SysLib.rawread(var4, var5);
-        var6 = var1 % 16 * 32;
-        System.arraycopy(var2, 0, var5, var6, 32);
-        SysLib.rawwrite(var4, var5);
-    }
-
-    int findIndexBlock() {
-        return this.indirect;
-    }
-
-    boolean registerIndexBlock(short var1) {
-        for(int var2 = 0; var2 < 11; ++var2) {
-            if (this.direct[var2] == -1) {
-                return false;
+            for (int i = 0; i < (Disk.blockSize / 2); i++) {
+                SysLib.short2bytes((short) -1, indexSetUp, i * 2);
             }
-        }
 
-        if (this.indirect != -1) {
-            return false;
-        } else {
-            this.indirect = var1;
-            byte[] var4 = new byte[512];
-
-            for(int var3 = 0; var3 < 256; ++var3) {
-                SysLib.short2bytes((short)-1, var4, var3 * 2);
-            }
-
-            SysLib.rawwrite(var1, var4);
+            SysLib.rawwrite(indexBlock, indexSetUp);
             return true;
         }
     }
 
-    int findTargetBlock(int var1) {
-        int var2 = var1 / 512;
-        if (var2 < 11) {
-            return this.direct[var2];
-        } else if (this.indirect < 0) {
+    /**
+     * @param iNumber
+     * @return
+     */
+    public int findTargetBlock(int iNumber) {
+        //SysLib.cout("\n**************** CALLING INODE FIND TARGET BLOCK *******************\n");
+        int indexNode = iNumber / Disk.blockSize;
+        if(indirect < 0) // if the indirect is not used
             return -1;
-        } else {
-            byte[] var3 = new byte[512];
-            SysLib.rawread(this.indirect, var3);
-            int var4 = var2 - 11;
-            return SysLib.bytes2short(var3, var4 * 2);
+        else if (indexNode < directSize)
+            return direct[indexNode];
+        else {
+            byte[] data = new byte[Disk.blockSize];
+            SysLib.rawread(indirect, data);
+            int node = indexNode - directSize;
+            return SysLib.bytes2short(data,node *2);
         }
     }
 
-    int registerTargetBlock(int var1, short var2) {
-        int var3 = var1 / 512;
-        if (var3 < 11) {
-            if (this.direct[var3] >= 0) {
+    /**
+     * @param iNumber
+     * @param i1
+     * @return
+     */
+    public int registerTargetBlock(int iNumber, short i1) {
+        //SysLib.cout("\n**************** CALLING INODE REGISTER TARGET BLOCK *******************\n");
+
+        if (iNumber == -1)  //check if indirect block can be used
+            return iNumber; //if so fail
+
+        int block = iNumber / Disk.blockSize;
+        if (direct[block] >= 0)
+            return -1;
+        else if (block > 0 && direct[block - 1] == -1) //if it is not the superblock and is free
+            return -1;
+        else {
+            //set up a byte array to be written to index block
+            byte[] indexSetUp = new byte[Disk.blockSize];
+            SysLib.rawread(indirect, indexSetUp);
+            int bInt = block - 11;
+            if (SysLib.bytes2short(indexSetUp, bInt * 2) > 0)
                 return -1;
-            } else if (var3 > 0 && this.direct[var3 - 1] == -1) {
-                return -2;
-            } else {
-                this.direct[var3] = var2;
-                return 0;
-            }
-        } else if (this.indirect < 0) {
-            return -3;
-        } else {
-            byte[] var4 = new byte[512];
-            SysLib.rawread(this.indirect, var4);
-            int var5 = var3 - 11;
-            if (SysLib.bytes2short(var4, var5 * 2) > 0) {
-                SysLib.cerr("indexBlock, indirectNumber = " + var5 + " contents = " + SysLib.bytes2short(var4, var5 * 2) + "\n");
-                return -1;
-            } else {
-                SysLib.short2bytes(var2, var4, var5 * 2);
-                SysLib.rawwrite(this.indirect, var4);
+            else {
+                SysLib.short2bytes(i1, indexSetUp, bInt * 2);
+                SysLib.rawwrite(indirect, indexSetUp);
                 return 0;
             }
         }
     }
 
-    byte[] unregisterIndexBlock() {
-        if (this.indirect >= 0) {
-            byte[] var1 = new byte[512];
-            SysLib.rawread(this.indirect, var1);
-            this.indirect = -1;
-            return var1;
-        } else {
-            return null;
+    /**
+     * @return
+     */
+    public byte[] unregisterIndexBlock() {
+        // SysLib.cout("\n**************** CALLING INODE UN-REGISTER INDEX BLOCK *******************\n");
+
+        if (indirect > -1) { //if it is actually being used
+            //set up a byte array to be written to index block
+            byte[] indexSetUp = new byte[Disk.blockSize];
+            SysLib.rawread(indirect, indexSetUp); //read up all the data inside the block
+            indirect = -1; //reset it to not being used
+            return indexSetUp;
         }
+        return null;
     }
 }
